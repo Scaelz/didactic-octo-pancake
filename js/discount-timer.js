c4d38839123cf465f3dc3d8ac80a8728ec093d71
Scaelz <avchursins@gmail.com>
2022-03-20 16:00:21 +0300

    add discount timer

---
+++ js/discount-timer.js
@@ -0,0 +1,36 @@
+const timer = document.querySelector('.timer__time');
+const deadline = '31 march 2022';
+
+const deadlineDate = new Date(deadline).getTime();
+
+
+const formatTimeUnit = (unit) => {
+    return unit < 10 ? '0' + unit : unit;
+}
+
+const calculateTimeUnits = (totalSeconds) => {
+    const seconds = Math.floor(totalSeconds % 60);
+    const minutes = Math.floor(totalSeconds / 60 % 60);
+    const hours = Math.floor(totalSeconds / 60 / 60 % 60);
+    const days = Math.floor(totalSeconds / 60 / 60 / 24)
+    return { seconds, minutes, hours, days };
+}
+
+const updateTimer = () => {
+    const currentDate = new Date().getTime();
+    const timeLeftInSeconds = (deadlineDate - currentDate) / 1000;
+    let {seconds, minutes, hours, days} = calculateTimeUnits(timeLeftInSeconds);
+    seconds = formatTimeUnit(seconds);
+    minutes = formatTimeUnit(minutes);
+    hours = formatTimeUnit(hours);
+    days = formatTimeUnit(days);
+    timer.textContent = `${days}:${hours}:${minutes}:${seconds}`;
+
+    if (timeLeftInSeconds < 0) {
+        clearInterval(interval);
+        timer.textContent = `00:00:00:00`;
+    }
+}
+
+const interval = setInterval(updateTimer, 500);
+
